8275213ca13c5af9f2d48772bff34a8b4a89a575
Adityagautam2004 <adikr2002@gmail.com>
2025-03-21 23:37:51 +0530

    form handling done

--- formHandling/src/App.jsx
+++ formHandling/src/App.jsx
@@ -1,15 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Form from './components/Form'
 import Cards from './components/cards'
 
 function App() {
    const[users , setUsers] = useState([])
+
+   const handleFormSubmitData = (data) => {
+      setUsers([...users, data])
+    }
+
+    const handleRemove = (id) => {
+      setUsers(() => users.filter((item, index) => index !== id))
+    }
+
   return (
     <>
      <div className='w-full h-screen bg-zinc-200 flex items-center jutify-center'>
       <div className='container mx-auto'>
-      <Cards users={users}/>    
-      <Form/>
+      <Cards users={users} handleRemove={handleRemove} />    
+      <Form handleFormSubmitData={handleFormSubmitData}/>
 
       </div>
      </div>
--- formHandling/src/components/Cards.jsx
+++ formHandling/src/components/Cards.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Card from './Card'
 
-function Cards() {
+function Cards({users,handleRemove}) {
   return (
     <div className='w-full max-h-96 overflow-auto  p-4 flex justify-center gap-4 flex-wrap'>
-      <Card />
+      {users.map((item, index) => {
+        return <Card user={item} id={index} key={index} handleRemove={handleRemove} />
+      })}
     </div>
   )
 }
