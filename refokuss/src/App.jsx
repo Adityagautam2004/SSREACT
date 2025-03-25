bc8ddbf6e9f96bdf901163d663054efce5a8ef8f
Adityagautam2004 <adikr2002@gmail.com>
2025-03-25 16:58:00 +0530

    Home section complete

--- refokuss/src/App.jsx
+++ refokuss/src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Work from './components/Work'
 
 function App() {
   return (
     <>
     <div className='w-full h-screen bg-zinc-900 font-["Poppins", sans-serif]'>
       <Navbar />
+      <Work />
     </div>
     </>
   )
