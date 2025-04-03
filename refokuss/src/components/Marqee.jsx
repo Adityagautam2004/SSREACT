67521b67a248d007f5079106559248e21bd09db0
Adityagautam2004 <adikr2002@gmail.com>
2025-04-03 19:38:33 +0530

    refokuss done

--- refokuss/src/components/Marqee.jsx
+++ refokuss/src/components/Marqee.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import {motion} from 'framer-motion'
 
-function Marqee({imagesurls}) {
+function Marqee({imagesurls , direction}) {
   return (
-    <div className='flex w-full py-8 gap-20 whitespace-nowrap overflow-hidden'>
-       {imagesurls.map(item => <img src={item} className="flex-shrink-0 w-[6vw]" />)}
-       {imagesurls.map(item => <img src={item} className="flex-shrink-0 w-[6vw]" />)}
+    <div className='flex flex w-full py-8  whitespace-nowrap overflow-hidden'>
+      <motion.div initial={{x:direction==='left' ? '0' : '-100%'}} animate={{x:direction==='left' ? '-100%' : '0'}} transition={{ease:"linear" , duration: 15 , repeat: Infinity} }
+      className='flex flex-shrink-0 gap-10 py-7 pl-10 '> 
+        {imagesurls.map((item, index) => <img key={index} src={item} alt='marqee' className='' />)}
+      </motion.div>
+      <motion.div initial={{x:direction==='left' ? '0' : '-100%'}} animate={{x:direction==='left' ? '-100%' : '0'}} transition={{ease:"linear" , duration: 15 , repeat: Infinity} }
+      className='flex flex-shrink-0 gap-10 py-7 pl-10'> 
+        {imagesurls.map((item, index) => <img key={index} src={item} alt='marqee' className='' />)}
+      </motion.div>
+ 
     </div>
   )
 }
